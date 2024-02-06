177ee5680e7d170631e8f13a25e47db05f83542a
Roberto Gutiérrez Rivera <roberto.gutierrez@uc.cl>
2024-02-05 22:10:09 -0300

    menu_inicio

---
+++ IIC2233/2023-1/T0/menu.py
@@ -0,0 +1,8 @@
+import os
+
+def main_menu() -> None:
+    print('*** Menú de Inicio ***\n')
+    file = input('Indique el nombre del archivo que desea abrir: ')
+
+def action_menu(file: str) -> None:
+    pass
